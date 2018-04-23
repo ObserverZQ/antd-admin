f95a21ec442eb702147b2c54ea39d2a7d807656f
ObserverZQ <657282348@qq.com>
2018-04-23 15:43:18 +0800

    模糊区间

--- antd-admin/src/routes/parameterSetting/index.js
+++ antd-admin/src/routes/parameterSetting/index.js
@@ -4,20 +4,37 @@ import { Page } from 'components'
 import styles from './index.less'
 
 const marks = {
-  0: '0°C',
+  0: {
+    style: {
+      color: '#1c4587',
+    },
+    label: <strong>0%</strong>,
+  },
+  40: {
+    style: {
+      color: '#073763',
+    },
+    label: <strong>40%</strong>,
+  },
+  60: {
+    style: {
+      color: '#073763',
+    },
+    label: <strong>60%</strong>,
+  },
   100: {
     style: {
       color: '#f50',
     },
     label: <strong>100%</strong>,
   },
-};
+}
 
 class Parameter extends React.Component {
   constructor () {
     super()
     this.state = {
-      range: [20, 70],
+      range: [40, 60],
     }
     this.onChange = this.onChange.bind(this)
     this.onAfterChange = this.onAfterChange.bind(this)
@@ -27,19 +44,19 @@ class Parameter extends React.Component {
     this.setState({
       range: value,
     })
-    console.log('onChange: ', value)
+    // console.log('onChange: ', value)
   }
   onAfterChange (value) {
     this.setState({
       range: value,
     })
-    console.log('onAfterChange: ', value)
+    // console.log('onAfterChange: ', value)
   }
   render () {
     return (<Page inner>
       <div className={styles.slider}>
         <h3>模糊区间</h3>
-        <Slider range marks={marks} step={5} defaultValue={this.state.range} onChange={this.onChange} onAfterChange={this.onAfterChange} />
+        <Slider range marks={marks} step={1} defaultValue={this.state.range} onChange={this.onChange} onAfterChange={this.onAfterChange} />
       </div>
     </Page>)
   }
